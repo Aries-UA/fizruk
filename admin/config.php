167f440e7d66ffeff7670975a17d92c8f40efc54
fizruk <fizruk@mitasych.com>
2012-10-08 20:35:02 +0300

    Добавлен нулевой уровень магазина

---
+++ admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://fizruk.com.ua/admin/');
+define('HTTP_CATALOG', 'http://fizruk.com.ua/');
+define('HTTP_IMAGE', 'http://fizruk.com.ua/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://fizruk.com.ua/admin/');
+define('HTTPS_CATALOG', 'http://fizruk.com.ua/');
+define('HTTPS_IMAGE', 'http://fizruk.com.ua/image/');
+
+// DIR
+define('DIR_APPLICATION', '/home/fizruk/data/www/fizruk.com.ua/admin/');
+define('DIR_SYSTEM', '/home/fizruk/data/www/fizruk.com.ua/system/');
+define('DIR_DATABASE', '/home/fizruk/data/www/fizruk.com.ua/system/database/');
+define('DIR_LANGUAGE', '/home/fizruk/data/www/fizruk.com.ua/admin/language/');
+define('DIR_TEMPLATE', '/home/fizruk/data/www/fizruk.com.ua/admin/view/template/');
+define('DIR_CONFIG', '/home/fizruk/data/www/fizruk.com.ua/system/config/');
+define('DIR_IMAGE', '/home/fizruk/data/www/fizruk.com.ua/image/');
+define('DIR_CACHE', '/home/fizruk/data/www/fizruk.com.ua/system/cache/');
+define('DIR_DOWNLOAD', '/home/fizruk/data/www/fizruk.com.ua/download/');
+define('DIR_LOGS', '/home/fizruk/data/www/fizruk.com.ua/system/logs/');
+define('DIR_CATALOG', '/home/fizruk/data/www/fizruk.com.ua/catalog/');
+
+// DB
+include "config_local.php";
+
+/*
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
+define('DB_PREFIX', '');
+*/
+
+?>
---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://fizruk.com.ua/');
+define('HTTP_IMAGE', 'http://fizruk.com.ua/image/');
+define('HTTP_ADMIN', 'http://fizruk.com.ua/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://fizruk.com.ua/');
+define('HTTPS_IMAGE', 'http://fizruk.com.ua/image/');
+
+// DIR
+define('DIR_APPLICATION', '/home/fizruk/data/www/fizruk.com.ua/catalog/');
+define('DIR_SYSTEM', '/home/fizruk/data/www/fizruk.com.ua/system/');
+define('DIR_DATABASE', '/home/fizruk/data/www/fizruk.com.ua/system/database/');
+define('DIR_LANGUAGE', '/home/fizruk/data/www/fizruk.com.ua/catalog/language/');
+define('DIR_TEMPLATE', '/home/fizruk/data/www/fizruk.com.ua/catalog/view/theme/');
+define('DIR_CONFIG', '/home/fizruk/data/www/fizruk.com.ua/system/config/');
+define('DIR_IMAGE', '/home/fizruk/data/www/fizruk.com.ua/image/');
+define('DIR_CACHE', '/home/fizruk/data/www/fizruk.com.ua/system/cache/');
+define('DIR_DOWNLOAD', '/home/fizruk/data/www/fizruk.com.ua/download/');
+define('DIR_LOGS', '/home/fizruk/data/www/fizruk.com.ua/system/logs/');
+
+// DB
+include "config_local.php";
+
+/*
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
+define('DB_PREFIX', '');
+*/
+
+?>
